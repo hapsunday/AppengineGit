9d905365c80e2e380a376587641feb16e1efe3af
Chris Reid <creid1992@gmail.com>
2011-11-30 12:44:14 +1100

    more simple bugs fixed due to the new python api

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ class Web(webapp2.RequestHandler):
 	def get(self):
 		self.response.out.write("Under Development")
 
-p = webapp2.WSGIApplication(
+app = webapp2.WSGIApplication(
 				[
 					('/([A-Za-z0-9]+).git(/.*)', GitRequest),
 					('/rpc/([A-Za-z\.]+)', rpc.Request),
